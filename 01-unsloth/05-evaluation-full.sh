0985fddf849c2ce2bbe47138d3c0c6ac3cd43083
Miroslav Hrabal <mhn@email.cz>
2024-03-20 02:00:19 +0100

    fix: python executable path

--- 01-unsloth/05-evaluation-full.sh
+++ 01-unsloth/05-evaluation-full.sh
@@ -7,7 +7,7 @@
 
 echo ${PBS_O_LOGNAME:?This script must be run under PBS scheduling system, execute: qsub $0}
 
-PYTHON=/storage/praha1/home/hrabalm/miniforge3/bin/python
+PYTHON=/storage/praha1/home/hrabalm/envs/unsloth/bin/python
 # SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )  # source: https://stackoverflow.com/questions/59895/how-do-i-get-the-directory-where-a-bash-script-is-located-from-within-the-script - broken on PBS, because the script is not run from the original directory
 SCRIPT_DIR="$PBS_O_WORKDIR"
 FILE="$SCRIPT_DIR/05-evaluation-full.py"
